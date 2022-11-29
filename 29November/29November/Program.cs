0730da2baf327f5952052fa26f4a7dbea74fa4e7
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2022-11-29 16:41:52 +0300

    new task

---
+++ 29November/29November/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29November
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int birthOfDate { get; set; }
+        public string Id { get; set; }
+
+        public Employee(string _Name, int _birthOfDate, string _Id)
+        {
+            Name = _Name;
+            birthOfDate = _birthOfDate;
+            Id = _Id;
+
+        }
+
+        public void BD (int year)
+        {
+            int birth = 2022 - year;
+            Console.WriteLine("Your Age : "+birth);
+        }
+
+        public void BD()
+        {
+            string month = Console.ReadLine();
+            Console.WriteLine(month);
+        }
+
+        public virtual void print()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(birthOfDate);
+            Console.WriteLine(Id);
+        }
+
+
+    
+    }
+
+
+
+    class Manager : Employee
+    {
+        public Manager(string _Name, int _birthOfDate, string _Id) : base(_Name, _birthOfDate, _Id)
+        {
+
+        }
+
+        public override void print()
+        {
+            Console.WriteLine("welcome "+Name);
+            Console.WriteLine("Your ID : "+Id);
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager details = new Manager("Mohammad", 1980, "785416");
+     
+            details.print();
+            details.BD(2001);
+            details.BD();
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleChoiceTests
+{
+    public interface ITestpaper
+    {
+        string Subject { get; }
+        string[] MarkScheme { get; }
+        string PassMark { get; }
+    }
+
+    public interface IStudent
+    {
+        string[] TestsTaken { get; }
+        void TakeTest(ITestpaper paper, string[] answers);
+    }
+
+    public class Testpaper : ITestpaper
+    {
+        public Testpaper(string subject, string[] markScheme, string passMark)
+        {
+            Subject = subject;
+            MarkScheme = markScheme;
+            PassMark = passMark;
+        }
+
+        public string Subject { get; private set; }
+
+        public string[] MarkScheme { get; private set; }
+
+        public string PassMark { get; private set; }
+    }
+
+    public class Student : IStudent
+    {
+        Dictionary<string, string> _results = new Dictionary<string, string>();
+
+        public string[] TestsTaken { get; private set; } = { "No tests taken" };
+
+        public void TakeTest(ITestpaper paper, string[] answers)
+        {
+            string pass = "Passed", fail = "Failed";
+            var score = (int)Math.Round((float)answers.Select((a, i) => a == paper.MarkScheme[i] ? 1 : 0).Sum() * 100 / paper.MarkScheme.Length);
+            var passMark = int.Parse(paper.PassMark.Substring(0, paper.PassMark.Length - 1));
+            _results.Add(paper.Subject, $"{(score >= passMark ? pass : fail)}! ({score}%)");
+            TestsTaken = _results.OrderBy(kvp => kvp.Key).Select(kvp => $"{kvp.Key}: {kvp.Value}").ToArray();
+        }
+    }
+}
